04c139aafc15266483cc81c8d08e0073bdf826b6
Kyle <kyle.w.stambaugh1@gmail.com>
2023-11-02 12:18:19 -0400

    started building login page

--- frontend_vmop/src/App.js
+++ frontend_vmop/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+
 
 function App() {
   
   return (
     <div className="App">
-      <>
-      </>
+      <Register/>
+
+
      
     </div>
   );
